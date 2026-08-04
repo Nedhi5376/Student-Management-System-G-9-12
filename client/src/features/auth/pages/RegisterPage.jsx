5e0b53f1ee125f82f597319a19b9cffbbbe64501
nedhi116 <nadhiijemal503@gmail.com>
2026-08-04 16:04:51 +0300

    Add comment explaining register page

--- client/src/features/auth/pages/RegisterPage.jsx
+++ client/src/features/auth/pages/RegisterPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { RegisterForm } from '../components/RegisterForm.jsx';
 
+// RegisterPage: public page hosting the account creation form.
 export function RegisterPage() {
   return (
     <main className="page">
