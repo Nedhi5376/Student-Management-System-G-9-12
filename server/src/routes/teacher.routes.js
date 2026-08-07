e9a9a81147ae697f7f42b0852bd5acc01df30a9d
nedhi116 <nadhiijemal503@gmail.com>
2026-08-08 00:27:19 +0300

    feat(teacher routes): require password change on first login for teachers

--- server/src/routes/teacher.routes.js
+++ server/src/routes/teacher.routes.js
@@ -11,10 +11,11 @@ import { asyncHandler } from '../middlewares/asyncHandler.js';
 import { validateBody } from '../middlewares/validate.js';
 import { verifyJWT } from '../middlewares/verifyJWT.js';
 import { verifyRole } from '../middlewares/verifyRole.js';
+import { requirePasswordChange } from '../middlewares/requirePasswordChange.js';
 import { attendanceSchema, marksSchema } from '../utils/validators.js';
 
 export const teacherRouter = Router();
-teacherRouter.use(verifyJWT, verifyRole('teacher'));
+teacherRouter.use(verifyJWT, verifyRole('teacher'), requirePasswordChange);
 
 teacherRouter.get('/assignments', asyncHandler(getMyAssignments));
 teacherRouter.get('/assignments/:id/roster', asyncHandler(getRoster));
