303f5accc1e1df491947fbf61e2bd6dfbc15f7e5
nedhi116 <nadhiijemal503@gmail.com>
2026-08-08 00:26:23 +0300

    feat(auth routes): add change-password endpoint, remove public register endpoint

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -4,9 +4,9 @@ import {
   logout,
   logoutAll,
   refresh,
-  register,
   verifyEmail,
   verifyMfaLogin,
+  changePassword,
 } from '../controllers/auth.controller.js';
 import {
   disableMfa,
@@ -17,18 +17,17 @@ import {
 import { asyncHandler } from '../middlewares/asyncHandler.js';
 import { validateBody } from '../middlewares/validate.js';
 import { verifyJWT } from '../middlewares/verifyJWT.js';
-import { loginLimiter, mfaLimiter, refreshLimiter, registerLimiter } from '../middlewares/rateLimiter.js';
+import { loginLimiter, mfaLimiter, refreshLimiter } from '../middlewares/rateLimiter.js';
 import {
   loginSchema,
   mfaDisableSchema,
   mfaEnableSchema,
-  registerSchema,
   totpSchema,
+  changePasswordSchema,
 } from '../utils/validators.js';
 
 export const authRouter = Router();
 
-authRouter.post('/register', registerLimiter, validateBody(registerSchema), asyncHandler(register));
 authRouter.get('/verify-email', asyncHandler(verifyEmail));
 authRouter.post('/login', loginLimiter, validateBody(loginSchema), asyncHandler(login));
 authRouter.post('/mfa/verify', mfaLimiter, validateBody(totpSchema), asyncHandler(verifyMfaLogin));
@@ -40,3 +39,5 @@ authRouter.post('/mfa/enable', verifyJWT, mfaLimiter, validateBody(mfaEnableSche
 authRouter.post('/mfa/disable', verifyJWT, mfaLimiter, validateBody(mfaDisableSchema), asyncHandler(disableMfa));
 authRouter.post('/mfa/backup-codes', verifyJWT, mfaLimiter, asyncHandler(regenerateBackupCodes));
 authRouter.post('/logout-all', verifyJWT, asyncHandler(logoutAll));
+
+authRouter.post('/change-password', verifyJWT, validateBody(changePasswordSchema), asyncHandler(changePassword));
